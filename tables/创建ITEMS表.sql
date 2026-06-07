ae5b1ce80927f8bbce800d02207453cec02ffd01
lilin <hqlthee@gmail.com>
2026-06-07 20:54:03 +0800

    创建道具表

---
+++ tables/创建ITEMS表.sql
@@ -0,0 +1,11 @@
+use POKEMON 
+go
+create table ITEMS
+(
+	ITEM_ID int not null identity(1,1),
+	ITEM_NAME nvarchar(50) not null,
+
+	constraint PK_ITEMS primary key (ITEM_ID),
+	constraint UNIQUE_ITEM unique (ITEM_NAME)
+)
+go
