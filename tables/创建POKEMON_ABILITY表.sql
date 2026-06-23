f98485919b1e7d90da54f26c891ebd62e194de4e
lilin <hqlthee@gmail.com>
2026-06-24 00:34:32 +0800

    debug

--- tables/创建POKEMON_ABILITY表.sql
+++ tables/创建POKEMON_ABILITY表.sql
@@ -4,8 +4,7 @@ create table POKEMON_ABILITY
 (
 	POKEMON_ABILITY_ID int identity(1,1) not null,
 	POKEMON_SPECIES_ID int not null,
-	ABILITY_1_ID int not null,
-    ABILITY_2_ID int null,
+	ABILITY_ID int not null,
 
 	 constraint PK_POKEMON_ABILITY
         primary key (POKEMON_ABILITY_ID),
@@ -17,8 +16,5 @@ create table POKEMON_ABILITY
         foreign key (POKEMON_SPECIES_ID) references POKEMON_SPECIES(POKEMON_SPECIES_ID),
 
     constraint FK_POKEMON_ABILITY_ABILITY_1
-        foreign key (ABILITY_1_ID) references ABILITIES(ABILITY_ID),
-
-    constraint FK_POKEMON_ABILITY_ABILITY_2
-        foreign key (ABILITY_2_ID) references ABILITIES(ABILITY_ID)
+        foreign key (ABILITY_ID) references ABILITIES(ABILITY_ID),
 )
