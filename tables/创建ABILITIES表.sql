c1c71d4d8b5053c0f0d9af39dd703458623c8a3c
lilin <hqlthee@gmail.com>
2026-06-23 23:28:06 +0800

    为特性和道具添加介绍

--- tables/创建ABILITIES表.sql
+++ tables/创建ABILITIES表.sql
@@ -4,6 +4,7 @@ create table ABILITIES
 (
 	ABILITY_ID int identity(1,1) not null,
 	ABILITY_NAME nvarchar(50) not null,
+	INTRODUCTION nvarchar(500),
 
 	constraint PK_ABILITIES primary key (ABILITY_ID),
 	constraint UNIQUE_ABILITIE_NAME unique (ABILITY_NAME)
--- tables/创建ITEMS表.sql
+++ tables/创建ITEMS表.sql
@@ -4,6 +4,7 @@ create table ITEMS
 (
 	ITEM_ID int not null identity(1,1),
 	ITEM_NAME nvarchar(50) not null,
+	INTRODUCTION nvarchar(500),
 
 	constraint PK_ITEMS primary key (ITEM_ID),
 	constraint UNIQUE_ITEM unique (ITEM_NAME)
